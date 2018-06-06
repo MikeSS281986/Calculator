f15be6b5e65f0b3e926a1f6842b714e7a1b81333
Mike Smith <MikeSS281986@users.noreply.github.com>
2018-06-06 09:13:30 -0500

    Update script.js
    Added buttons disabled feature on page load. Only come on when ON button is clicked.

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-function turnOn() { document.getElementById("display").style.backgroundColor = "white"; }
-function reset() { document.getElementById("calculator").reset(); }
+
+function turnOn() {
+  document.getElementById("display").style.backgroundColor = "white";
+  document.getElementsByName("square")[0].disabled = false;
+  document.getElementsByName("division")[0].disabled = false;
+  document.getElementsByName("multiply")[0].disabled = false;
+  document.getElementsByName("seven")[0].disabled = false;
+  document.getElementsByName("eight")[0].disabled = false;
+  document.getElementsByName("nine")[0].disabled = false;
+  document.getElementsByName("minus")[0].disabled = false;
+  document.getElementsByName("four")[0].disabled = false;
+  document.getElementsByName("five")[0].disabled = false;
+  document.getElementsByName("six")[0].disabled = false;
+  document.getElementsByName("add")[0].disabled = false;
+  document.getElementsByName("one")[0].disabled = false;
+  document.getElementsByName("two")[0].disabled = false;
+  document.getElementsByName("three")[0].disabled = false;
+  document.getElementsByName("zero")[0].disabled = false;
+  document.getElementsByName("decimal")[0].disabled = false;
+}
+function reset() { 
+  document.getElementById("calculator").reset(); 
+}
